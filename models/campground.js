f4113f28d0c8b9f69d023306d2bff39b3dec7504
Ishaan-10 <b.ishaan10@gmail.com>
2021-04-11 19:27:36 +0530

    Added styling

--- models/campground.js
+++ models/campground.js
@@ -2,19 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title:{
-        type:String
-    },
-    price:{
-        type:Number,
-    },
-    description:{
-        type:String,
-    },
-    location:{
-        type:String
-    }
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String
+});
 
-})
-
-module.exports = mongoose.model('Campground',CampgroundSchema);
+module.exports = mongoose.model('Campground', CampgroundSchema);
